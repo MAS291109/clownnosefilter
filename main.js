11b70c8d5074272fd53762220a704b1d2fc5094e
MAS291109 <81005913+MAS291109@users.noreply.github.com>
2021-12-24 12:34:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,34 @@
 function preload(){
 }
 
-function setup() {
-    canvas = createCanvas(500,400);
-    canvas.position(70,200);
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
     video = createCapture(VIDEO);
+    video.size(300, 300);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
-function draw() {
-    image(video,100,80,300,250);
 
-    stroke(168, 10, 10);
-    fill(250, 191, 201);
-    
-    circle(40,40,50); 
-    rect(230, 15, 55, 55, 20);
-    circle(460,40,50);
-    rect(432, 170, 55, 55, 20);
-    circle(40,360,50);
-    rect(12, 170, 55, 55, 20);
-    circle(460,360,50);
-    rect(230, 333, 55, 55, 20);
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
 
+function gotPoses(result)
+{
+    if(result.length > 0){
+        console.log(result);
+        console.log("nose x - " +result[0].pose.nose.x);
+        console.log("nose y - " +result[0].pose.nose.y);
+    }
+}
 
-    stroke(50, 178, 82);
-    fill(0,0,0);
-
-    rect(63, 27, 375, 25);
-    rect(63, 347, 375, 25);
-    rect(27, 63, 25, 275);
-    rect(448, 63, 25, 275);
+function draw(){    
+    image(video, 0, 0, 300, 300);
 }
 
 function take_snapshot(){
-    save("frame_picture.png");
+    save('clownnoseFilterImg.png');
 }
